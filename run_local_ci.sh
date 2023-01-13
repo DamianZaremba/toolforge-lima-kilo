55a608f5ff3fe83e8129dd75bd9966f78668e064
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2023-01-13 14:02:16 +0100

    run_local_ci.sh: refresh file
    Base it on a docker image, the same we will use for CI.
    
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

---
+++ run_local_ci.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+set -e
+
+BASE_DIR=$(dirname $(realpath -s $0))
+CONTAINER="ansible-lint"
+IMAGE_NAME="docker-registry.tools.wmflabs.org/cloud-cicd-py39bullseye-ansible:latest"
+
+exit_trap() {
+  docker rm -f $CONTAINER
+}
+
+set -x
+trap 'exit_trap' EXIT
+
+set -x
+docker run \
+    --name $CONTAINER \
+    --volume ${BASE_DIR}:/src \
+    $IMAGE_NAME \
+    /bin/sh -c -- cd /src ; ansible-lint
