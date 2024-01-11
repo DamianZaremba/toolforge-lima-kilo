4fa6d705abf3fe36e1a19f8278cce2cf4a37ac07
David Caro <dcaro@wikimedia.org>
2024-01-11 15:58:29 +0100

    Remove tekton repo
    We don't need it anymore, we don't use tekton-cli
    
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- lima-vm/prepare.sh
+++ lima-vm/prepare.sh
@@ -27,11 +27,6 @@ sudo bash -c "cat >/etc/apt/sources.list.d/toolforge.list<<EOR
 deb [trusted=yes] https://deb-tools.wmcloud.org/repo bullseye-tools main
 EOR"
 
-# this will not be needed once https://gitlab.wikimedia.org/repos/cloud/toolforge/builds-cli/-/merge_requests/8 is deployed
-sudo bash -c "cat >/etc/apt/sources.list.d/tekton.list<<EOR
-deb [trusted=yes] http://apt.wikimedia.org/wikimedia buster-wikimedia thirdparty/tekton
-EOR"
-
 sudo apt update
 sudo apt install -y toolforge-cli toolforge-builds-cli toolforge-jobs-framework-cli toolforge-envvars-cli toolforge-webservice
 
