57fa45f2a8408b85d096d1e1ee77cdfe6922e4fa
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2024-05-07 13:52:32 +0200

    helpers: add toolforge_foxtrot_ldap_load_users.sh
    In some scenarios it could be useful to reload foxtrot-ldap data.
    Add a script to do it.
    
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

---
+++ helpers/toolforge_foxtrot_ldap_load_users.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# NOTE:
+# this script should help reloading foxtrot-ldap users after a lima-vm reboot
+# given foxtrot-ldap doesn't have persistent storage, and restarting the foxtrot-ldap
+# container will result in a cleanup of the LDAP tree
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -v
+
+toolforge_run_ansible.sh -D --tags foxtrot_ldap
