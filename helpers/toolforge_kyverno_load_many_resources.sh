825be38c17b849745d878e593b2d90eb7e0ac466
Arturo Borrero Gonzalez <aborrero@wikimedia.org>
2024-06-20 12:10:11 +0200

    helpers/toolforge_kyverno_load_many_resources.sh: be more realistic
    Create the load distributed across namespaces to more accurately resemble
    what is happening in the actual toolforge.
    
    Before this patch, the 4000 policy resources were being created on the same
    namespace.
    
    Bug: T367386
    Signed-off-by: Arturo Borrero Gonzalez <aborrero@wikimedia.org>

--- helpers/toolforge_kyverno_load_many_resources.sh
+++ helpers/toolforge_kyverno_load_many_resources.sh
@@ -7,24 +7,22 @@ HOW_MANY="4000"
 TEMPLATE_URL="https://gitlab.wikimedia.org/repos/cloud/toolforge/maintain-kubeusers/-/raw/main/maintain_kubeusers/resources/kyverno_pod_policy.yaml.tpl?ref_type=heads&inline=false"
 TMP_DIR=$(mktemp --directory)
 POLICY_TEMPLATE="${TMP_DIR}/policy_template.yaml"
-POLICY_BASE="${TMP_DIR}/policy_base.yaml"
 
-NAMESPACE="tool-tf-test"
-TOOL_DATA_DIR="/data/project/tf-test"
-TOOL_UID="50001"
-NAME="toolforge-kyverno-pod-policy"
+BASE_NAMESPACE="tool-tf-test"
+BASE_TOOL_DATA_DIR="/data/project/tf-test"
+BASE_TOOL_UID="50001"
 
 wget "${TEMPLATE_URL}" -O "${POLICY_TEMPLATE}"
 
-sed -e "s@\${TOOL_DATA_DIR}@$TOOL_DATA_DIR@g" \
-    -e "s@\${NAMESPACE}@$NAMESPACE@g" \
-    -e "s@\${TOOL_UID}@$TOOL_UID@g" "${POLICY_TEMPLATE}" > "${POLICY_BASE}"
-
-kubectl delete namespace "${NAMESPACE}" || true
-kubectl create namespace "${NAMESPACE}" || true
-
 for i in $(seq 1 "${HOW_MANY}") ; do
+    ns="${BASE_NAMESPACE}-${i}"
+    data_dir="${BASE_TOOL_DATA_DIR}${i}"
+    tool_uid="${BASE_TOOL_UID}${i}"
+
+    kubectl create namespace "${ns}" || true
     # shellcheck disable=SC2140
-    sed -e s@"name: \"${NAME}\""@"name: \"${NAME}-${i}\""@g "${POLICY_BASE}" | \
-        kubectl -n "${NAMESPACE}" apply -f -
+    sed -e "s@\${TOOL_DATA_DIR}@$data_dir@g" \
+        -e "s@\${TOOL_UID}@$tool_uid@g" \
+        -e "s@\${NAMESPACE}@$ns@g" "${POLICY_TEMPLATE}" | \
+        kubectl -n "${ns}" apply -f -
 done
