b72a26b0e7d136a15dd1d8e05ddee3aeff010cb8
raymond-ndibe <rndibe@wikimedia.org>
2024-10-16 00:15:23 +0100

    [lima-kilo] fix unbound variable bug in start-devenv.sh
    start-devenv.sh throws unbound variable error
    if executed without `--`.
    This fixes that.
    
    Signed-off-by: raymond-ndibe <rndibe@wikimedia.org>

--- start-devenv.sh
+++ start-devenv.sh
@@ -178,7 +178,7 @@ main() {
     # the hostname contains the `lima-` prefix by default, see https://github.com/lima-vm/lima/discussions/1634
     # override it to remove the duplicated `lima` keyword
     limactl shell lima-kilo -- sudo hostnamectl hostname lima-kilo
-    limactl shell lima-kilo -- ./lima-vm/install.sh "${ansible_args[@]}"
+    limactl shell lima-kilo -- ./lima-vm/install.sh "${ansible_args[@]+"${ansible_args[@]}"}"
     if [[ "$COPY_SRC" != "" ]]; then
         if [[ "$RECURSIVE" == "true" ]]; then
             copy_files_recursive "$COPY_SRC"
