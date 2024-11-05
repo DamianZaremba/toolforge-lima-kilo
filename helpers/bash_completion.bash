417c120117723490e4a350913d0c6b0648ed9a34
David Caro <dcaro@wikimedia.org>
2024-11-05 11:53:47 +0100

    toolforge_deploy_mr: add basic autocompletion
    Signed-off-by: David Caro <dcaro@wikimedia.org>

---
+++ helpers/bash_completion.bash
@@ -0,0 +1,26 @@
+# Bash completion functions for helpers inside lima-kilo
+COMPONENTS_DIR=$HOME/toolforge-deploy/components
+
+
+_toolforge_deploy_mr.py() {
+    # only complete the component name for now
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	COMPREPLY=()
+
+	local cur_index="$COMP_CWORD"
+
+    local components
+    components="$(ls "$COMPONENTS_DIR")"
+
+	case "$cur_index" in
+		1)
+			if [[ $cur == -* ]]; then
+				mapfile -t COMPREPLY < <(compgen -W "--help" -- "${cur}")
+			else
+				mapfile -t COMPREPLY < <(compgen -W "$components" -- "${cur}")
+			fi
+			;;
+	esac
+}
+
+complete -F _toolforge_deploy_mr.py toolforge_deploy_mr.py
