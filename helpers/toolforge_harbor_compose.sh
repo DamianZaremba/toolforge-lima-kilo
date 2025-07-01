484428823c0c243a75fc7ccbb229878b5dd44583
raymond-ndibe <rndibe@wikimedia.org>
2025-07-01 15:08:31 +0100

    [docker-compose] use `docker compose` instead of docker-compose
    Bug: T398322
    Signed-off-by: raymond-ndibe <rndibe@wikimedia.org>

--- helpers/toolforge_harbor_compose.sh
+++ helpers/toolforge_harbor_compose.sh
@@ -4,4 +4,4 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-sudo docker-compose -f "/srv/ops/harbor/harbor/docker-compose.yml" "$@"
+sudo docker compose -f "/srv/ops/harbor/harbor/docker-compose.yml" "$@"
