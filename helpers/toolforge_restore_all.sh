75f96be5d486fd96de6a485916d17e3a1c7a088d
David Caro <dcaro@wikimedia.org>
2025-10-08 15:03:18 +0200

    helpers: add script to restore all components
    Got tired of going one-by-one :)
    
    Signed-off-by: David Caro <dcaro@wikimedia.org>

---
+++ helpers/toolforge_restore_all.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+
+help() {
+    cat <<EOH
+    Usage $0 [-r|-h]
+
+    Description:
+        This restores all the components that have been modified, or out of sync with the toolforge-deploy repository under ~/toolforge-deploy
+
+        It uses toolforge_get_versions.sh and toolforge_deploy_mr.py
+
+    Options:
+        -h
+            Show this help
+
+        -r
+            If passed, it will git fetch && git reset --hard FETCH_HEAD the toolforge-deploy repository getting the latest changes.
+EOH
+}
+
+
+get_modified_k8s_components() {
+    toolforge_get_versions.sh \
+    | grep -e '\(mr:\|toolforge-deploy has\)' \
+    | awk '{print $2}'
+}
+
+main() {
+    if [[ "${1:-}" == "-h" ]]; then
+        help
+        exit 0
+    fi
+    if [[ "${1:-}" == "-r" ]]; then
+        echo "Updating toolforge-deploy to latest"
+        cd ~/toolforge-deploy
+        git fetch --all && git reset --hard FETCH_HEAD
+    fi
+    for component in $(get_modified_k8s_components); do
+        echo "### Restoring component $component"
+        # the heredoc is to avoid it asking for confirmation
+        toolforge_deploy_mr.py "$component" restore <<<""
+    done
+}
+
+
+main "$@"
